bbc1555c0cac904b28616a1f6a5be2ea623c90db
shahabas <shezilaman@gmail.com>
2024-02-15 15:11:02 +0530

    MongoDB

---
+++ Schema/UserModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+// Define user schema
+const userSchema = new mongoose.Schema({
+  UserID: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  Username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  Password: {
+    type: String,
+    required: true
+  },
+  Posts: {
+    type: Number,
+    required: true
+  },
+  Streak:{
+    type: Number,
+    required: true
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
