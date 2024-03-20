737879b249051df4faa794cfd7293c6fafa2ed2d
shahabas <shezilaman@gmail.com>
2024-03-20 10:59:41 +0530

    merged userValidation

--- BackEnd/Schema/UserModel.js
+++ BackEnd/Schema/UserModel.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-// Define user schema
 const userSchema = new mongoose.Schema({
   Username: {
     type: String,
@@ -29,6 +28,21 @@ const userSchema = new mongoose.Schema({
     default: 0
   }
 });
+<<<<<<< Updated upstream
+=======
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if (!user.isModified('Password')) return next();
+
+  try {
+    const hashedPassword = await bcrypt.hash(user.Password, 10);
+    user.Password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+>>>>>>> Stashed changes
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
